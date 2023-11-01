45ebf23a937aaec8392ffbd885b41410df5f5c49
Francisco Andrade <francisco.andrade@tolife.com.br>
2023-10-31 23:20:16 -0300

    final version

--- arrays-hashing/product_except_self.py
+++ arrays-hashing/product_except_self.py
@@ -2,52 +2,28 @@ from typing import List
 
 
 def product_except_self(nums: List[int]):
-    def product(numbers: List[int]) -> int | None:
-        if len(numbers) == 0:
-            return None
-
-        if len(numbers) == 1:
-            return numbers[0]
-
-        value = numbers[0]
-        for num in numbers[1:]:
-            value *= num
-        return value
-
     def find_prefix(numbers: List[int]) -> List[int]:
-        prefix = [numbers[0]]
-        i = 1
+        prefix = [1]
+        i = 0
 
         while i < len(numbers):
-            prefix.append(numbers[i - 1] * prefix[i - 1])
+            prefix.append(numbers[i] * prefix[i])
             i += 1
-
-        prefix[0] = 1
 
         return prefix
 
     def find_postfix(numbers: List[int]) -> List[int]:
-        postfix = [0] * (len(numbers) - 1)
+        postfix = [1] * (len(numbers) - 1)
         postfix.append(1)
-        i = len(numbers) - 2
+        i = len(numbers) - 1
 
-        while i >= 0:
-            postfix[i] = numbers[i + 1] * postfix[i + 1]
+        while i > 0:
+            postfix[i - 1] = numbers[i] * postfix[i]
             i -= 1
 
         postfix[-1] = 1
 
         return postfix
-
-    def zero_indices(numbers: List[int]) -> tuple:
-        zero_indices = []
-        for index, value in enumerate(numbers):
-            if value == 0:
-                zero_indices[0].append(index)
-        return zero_indices
-
-    # prefix = find_prefix(nums)
-    # postfix = find_postfix(nums)
 
     products = [1] * len(nums)
 
