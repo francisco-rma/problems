bf27a53fd98b3e95649c88582d072a0e111d7794
Francisco Andrade <francisco.andrade@tolife.com.br>
2024-04-03 17:45:53 -0300

    wip: tree structuring

--- trees/bst.py
+++ trees/bst.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+from typing import Sequence
+
+
 class Node(object):
 
     def __init__(self, value=0, left=None, right=None) -> None:
@@ -12,9 +16,7 @@ class Node(object):
             self.right = right
         pass
 
-    def search(self, target):
-        print(f"Value: {self.value}")
-        children = [child.value for child in self.children]
+    def search(self, target: int):
         if self.value == target:
             return self
         elif self.children is None or len(self.children) <= 0:
@@ -27,8 +29,79 @@ class Node(object):
                 else:
                     return test
 
+    def __eq__(self, comparison: Node) -> bool:
+        if self.value != comparison.value:
+            return False
+        left_validation = self.left.__eq__(comparison.left)
+        right_validation = self.right.__eq__(comparison.right)
+
+        return left_validation and right_validation
+
     def add_child(self, child):
-        if isinstance(child, Tree):
+        if isinstance(child, Node):
             self.children.append(child)
         else:
-            self.children.append(Tree(child))
+            self.children.append(Node(child))
+
+
+def populate_bst(values: Sequence[int], inverted=False) -> Node:
+    n = len(values)
+
+    if n == 0:
+        return None
+
+    def inner(index: int = 0):
+        if index >= n or values[index] is None:
+            return None
+
+        node = Node(values[index])
+
+        max_idx = None
+        min_idx = None
+
+        if 2 * index + 1 >= n:
+            min_idx = 2 * index + 1
+            max_idx = 2 * index + 2
+
+        elif values[2 * index + 1] > values[2 * index + 2]:
+            min_idx = 2 * index + 2
+            max_idx = 2 * index + 1
+
+        else:
+            min_idx = 2 * index + 1
+            max_idx = 2 * index + 2
+
+        if inverted:
+            min_idx, max_idx = max_idx, min_idx
+
+        node.left = inner(min_idx)
+        node.right = inner(max_idx)
+
+        return node
+
+    return inner()
+
+
+def generate_root(tree: Node, inverted=False) -> Sequence[int]:
+    root = []
+    root.append(tree.value)
+
+    def inner(node: Node):
+        has_left_node = node.left is not None
+        has_right_node = node.right is not None
+
+        if has_left_node:
+            root.append(node.left.value)
+
+        if has_right_node:
+            root.append(node.right.value)
+
+        if has_left_node:
+            inner(node.left)
+
+        if has_right_node:
+            inner(node.right)
+
+    inner(tree)
+
+    return root
--- trees/invert-bst.py
+++ trees/invert-bst.py
@@ -1,36 +1,15 @@
-from typing import Sequence
-from bst import Node
+from bst import Node, generate_root, populate_bst
 
 MAX_LVL = 3
 
 
-def invert_bst(root: Node) -> Node | None:
+def invert_bst(root: Node) -> Node:
     if root.left == root.right == None:
         return
     root.left, root.right = root.right, root.left
     invert_bst(root.left)
     invert_bst(root.right)
     return root
-
-
-def populate_bst(values: Sequence[int]) -> Node:
-    n = len(values)
-
-    if n == 0:
-        return None
-
-    def inner(index: int = 0):
-        if index >= n or values[index] is None:
-            return None
-
-        node = Node(values[index])
-
-        node.left = inner(2 * index + 1)
-        node.right = inner(2 * index + 2)
-
-        return node
-
-    return inner()
 
 
 values = [4, 2, 7, 1, 3, 6, 9]
@@ -45,5 +24,14 @@ testBst.right.left = Node(6)
 testBst.right.right = Node(9)
 
 tree = populate_bst(values=values)
+inverted_tree = populate_bst(values=values, inverted=True)
 
-print(tree)
+root = generate_root(tree)
+inverted_root = generate_root(inverted_tree)
+
+
+validation1 = tree == testBst
+validation2 = inverted_root == [4, 7, 2, 9, 6, 3, 1]
+
+print(f"Validation 1: {validation1}")
+print(f"Validation 2: {validation2}")
