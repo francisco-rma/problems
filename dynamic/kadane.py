a906424036b2706a37036abe9917f8af33c40ebe
Francisco Andrade <francisco.andrade@tolife.com.br>
2024-08-16 13:56:52 -0300

    kadane: add index ranges

--- dynamic/kadane.py
+++ dynamic/kadane.py
@@ -3,17 +3,27 @@ def largest_sum(numbers: list[int]) -> int:
         raise ValueError("Empty list")
 
     i = 1
+    min_idx = 0
     max_sum = numbers[0]
     current_sum = numbers[0]
+    window = [0, 0]
 
     while i < len(numbers):
         current_sum = max(current_sum + numbers[i], numbers[i])
-        max_sum = max(current_sum, numbers[i], max_sum)
+        if current_sum == numbers[i]:
+            min_idx = i
+
+        max_sum = max(current_sum, max_sum)
+        if max_sum == current_sum:
+            window[0], window[1] = min_idx, i
+
         i += 1
 
-    return max_sum
+    return max_sum, window
 
 
 array = [4, -1, 2, -7, 3, 4]
-result = largest_sum(array)
+result, window = largest_sum(array)
 print(f"Largest sum: {result}")
+print(f"Window: {window}")
+print(f"Window value: {array[window[0]:window[1]+1]}")
