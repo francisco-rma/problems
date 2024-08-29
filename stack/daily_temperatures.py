710124bde0732cbdc0fe515fefde7f9557655830
Francisco Andrade <frma059@gmail.com>
2024-08-29 19:29:35 -0300

    stack: daily temperatures refactor

--- stack/daily_temperatures.py
+++ stack/daily_temperatures.py
@@ -2,22 +2,12 @@ def dailyTemperatures(temperatures: list[int]) -> list[int]:
     stack = []
     result = [0] * len(temperatures)
 
-    i = 0
-
-    while i <= len(temperatures) - 1:
-        current_temp = temperatures[i]
-
-        if not stack or current_temp <= temperatures[stack[-1]]:
-            stack.append(i)
-            i += 1
-            continue
-
-        while stack and current_temp > temperatures[stack[-1]]:
-            result[stack[-1]] = i - stack[-1]
+    for idx, temp in enumerate(temperatures):
+        while stack and temp > temperatures[stack[-1]]:
+            result[stack[-1]] = idx - stack[-1]
             stack.pop()
 
-        stack.append(i)
-        i += 1
+        stack.append(idx)
 
     return result
 
