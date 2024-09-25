eaed7f432c8fb20007599e1a3b08f4f113e80d27
Francisco Andrade <frma059@gmail.com>
2024-09-24 21:17:51 -0300

    binary search: median of rotated arrays

--- search/sorted_arrays_median.py
+++ search/sorted_arrays_median.py
@@ -1,73 +1,61 @@
-def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
-    def binary_count(values: list[int], target: int) -> int:
-        if not values or values[-1] < target:
-            None
+def binary_count(values: list[int], target: int) -> int:
+    if not values or values[-1] < target:
+        None
 
-        left = 0
+    left = 0
 
-        right = len(values) - 1
-
-        while left <= right:
-            midpoint = left + ((right - left) // 2)
-
-            if values[midpoint] <= target and values[midpoint + 1] > target:
-                return midpoint
-
-            elif values[midpoint] < target:
-                left = midpoint + 1
-
-            else:
-                right = midpoint - 1
-
-        return 0
-
-    first = None
-    second = None
-
-    if nums1[0] < nums2[0]:
-        first = nums1
-        second = nums2
-
-    elif nums1[0] == nums2[0]:
-        if nums1[-1] <= nums2[-1]:
-            first = nums1
-            second = nums2
-    else:
-        first = nums2
-        second = nums1
-
-    m, n = len(first), len(second)
-
-    left, right = 0, n + m - 1
-
-    midpoint = left + ((right - left) // 2)
+    right = len(values) - 1
 
     while left <= right:
-        idx1 = binary_count(values=first, target=midpoint)
-        idx2 = binary_count(values=second, target=midpoint)
+        midpoint = left + ((right - left) // 2)
 
-        left_count1, right_count1 = idx1 + 1, len(first) - (idx1 + 1)
-        left_count2, right_count2 = idx2 + 1, len(second) - (idx2 + 1)
+        if values[midpoint] <= target and values[midpoint + 1] > target:
+            return midpoint
 
-        diff = abs((left_count1 + left_count2) - (right_count1 + right_count2)) <= 1
-
-        if diff == 0:
-            res = 0
-            if midpoint < m:
-                res = first[midpoint]
-            return
-
-        elif diff == 1:
-            res1 = 0
-            res2 = 0
-            if midpoint < m:
-                res1 = first[[midpoint]]
-                pass
-
-        elif diff > 1:
-            pass
+        elif values[midpoint] < target:
+            left = midpoint + 1
 
         else:
-            pass
+            right = midpoint - 1
 
-    pass
+    return 0
+
+
+def findMedianSortedArrays(nums1: list[int], nums2: list[int]) -> float:
+    A, B = nums1, nums2
+
+    if len(A) > len(B):
+        A, B = B, A
+
+    half = (len(A) + len(B)) // 2
+
+    left, right = 0, len(A) - 1
+
+    while True:
+        i = left + ((right - left) // 2)
+        j = half - i - 2
+
+        leftA = A[i] if i >= 0 else float("-infinity")
+        leftB = B[j] if j >= 0 else float("-infinity")
+        rightA = A[i + 1] if (i + 1) < len(A) else float("infinity")
+        rightB = B[j + 1] if (j + 1) < len(B) else float("infinity")
+
+        if leftA > rightB:
+            right = i - 1
+
+        elif leftB > rightA:
+            left = i + 1
+
+        else:
+            if (len(A) + len(B)) % 2 == 0:
+                return (max(leftA, leftB) + min(rightA, rightB)) / 2
+            else:
+                return min(rightA, rightB)
+
+
+nums1 = [1, 2]
+nums2 = [3]
+
+result = findMedianSortedArrays(nums1=nums1, nums2=nums2)
+
+print(result)
