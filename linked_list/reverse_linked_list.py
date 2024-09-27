41bc4ffdb3856d4de04b11c89c443255637fe25e
Francisco Andrade <frma059@gmail.com>
2024-09-27 13:26:37 -0300

    linked list: reverse linked list

---
+++ linked_list/reverse_linked_list.py
@@ -0,0 +1,76 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def from_list(self, values: list[int]):
+        val = self
+        for i, value in enumerate(values):
+            if val is not None:
+                val.val = value
+
+            if i < len(values) - 1:
+                val.next = ListNode()
+            val = val.next
+
+
+def reverseListStack(head: Optional[ListNode]) -> Optional[ListNode]:
+    if head is None:
+        return None
+
+    stack = []
+    node = head
+
+    while node is not None:
+        stack.append(node.val)
+        node = node.next
+
+    node = head
+
+    while stack:
+        if node is not None:
+            node.val = stack.pop()
+            node = node.next
+
+    return head
+
+
+def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+    if head is None:
+        return None
+
+    prev_node = None
+    cur_node = head
+
+    while cur_node is not None:
+        next_node = cur_node.next
+
+        if prev_node is not None:
+            cur_node.next = prev_node
+
+        prev_node = cur_node
+        cur_node = next_node
+
+    head = prev_node
+    return head
+
+
+def reverseListRecursive(head: Optional[ListNode]) -> Optional[ListNode]:
+
+    if head is None:
+        return None
+
+    pass
+
+
+source = [0, 1, 2, 3]
+
+head = ListNode(0)
+head.from_list(source)
+
+result = reverseList(head=head)
+
+print(result)
