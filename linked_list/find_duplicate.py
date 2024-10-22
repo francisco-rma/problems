11f3b327e99ce6521faf13ca4d5f400b26a0c239
Francisco Andrade <frma059@gmail.com>
2024-10-21 23:48:29 -0300

    linked list: find duplicate | WIP

---
+++ linked_list/find_duplicate.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+
+class ListNode:
+    def __init__(self, val: int, next: ListNode = None):
+        self.val = val
+        self.next = next
+
+    def from_list(values: list[int]):
+        head = ListNode(0)
+        node = head
+        for idx, val in enumerate(values):
+            node.val = val
+
+            if idx < len(values) - 1:
+                node.next = ListNode(0)
+
+            node = node.next
+        return head
+
+    def findDuplicate(self) -> int:
+        counts = {}
+
+        node = self
+
+        while node is not None:
+            if node.val in counts.keys():
+                return node.val
+
+            counts[node.val] = 1
+
+            node = node.next
+
+        return False
+
+
+nums = [1, 2, 3, 4, 4]
+ll: ListNode = ListNode.from_list(nums)
+
+print(ll.findDuplicate())
