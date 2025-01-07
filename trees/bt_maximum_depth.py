fdfbb77007c479d201b996a33368481dc39ca50d
Francisco Andrade <frma059@gmail.com>
2025-01-07 18:00:03 -0300

    trees: binary tree maximum depth with iterative dfs

--- trees/bt_maximum_depth.py
+++ trees/bt_maximum_depth.py
@@ -24,25 +24,68 @@ class TreeNode:
 
         return inner()
 
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
+    def maxDepth(self, root: Optional[TreeNode], method: str = "dfsr") -> int:
         if not root:
             return 0
 
-        def dfs(node: Optional[TreeNode], level: int = 0):
-            max_depth = level + 1
-            if not (node.left or node.right):
-                print(f"Max depth so far is {max_depth}")
-                return max_depth
-            if node.left:
-                max_depth = max(max_depth, dfs(node=node.left, level=level + 1))
-            if node.right:
-                max_depth = max(max_depth, dfs(node=node.right, level=level + 1))
+        match method:
+            case "dfsr":
 
-            return max_depth
+                def dfsr(node: Optional[TreeNode]):
+                    if not node:
+                        return 0
+                    return 1 + max(dfsr(node=node.left), dfsr(node=node.right))
 
-        return dfs(root, level=0)
+                return dfsr(root)
+
+            case "dfsi":
+
+                def dfsi(node: Optional[TreeNode]):
+                    if not node:
+                        return 0
+
+                    from collections import deque
+
+                    max_depth = 1
+                    cur_depth = 1
+                    visited: set[TreeNode] = set([node])
+                    stack: deque[tuple[TreeNode, int]] = deque()
+                    stack.append((node, cur_depth))
+
+                    while stack:
+                        if node and node not in visited:
+                            cur_depth += 1
+                            visited.add(node)
+
+                        is_leaf = not (node.left or node.right)
+
+                        if is_leaf:
+                            max_depth = max(max_depth, cur_depth)
+                            node, cur_depth = stack.pop()
+                            continue
+
+                        if node.right and node.right not in visited:
+                            stack.append((node, cur_depth))
+                            node = node.right
+                            continue
+
+                        if node.left and node.left not in visited:
+                            stack.append((node, cur_depth))
+                            node = node.left
+                            continue
+
+                        node, cur_depth = stack.pop()
+
+                    return max_depth
+
+                return dfsi(root)
+
+            case _:
+                return None
+
+    def maxDepthBfs(self, root: Optional[TreeNode]): ...
 
 
 root: TreeNode = TreeNode.from_list([1, 2, 3, 4, 5])
 
-print(root.maxDepth(root=root))
+print(root.maxDepth(root=root, method="dfsi"))
