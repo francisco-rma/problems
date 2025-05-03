cacb2edcaa009b8106ff0ddc616267ab2974ac94
Francisco Andrade <frma059@gmail.com>
2025-05-03 14:17:49 -0300

    implementing max heap

--- heaps/heap.py
+++ heaps/heap.py
@@ -1,15 +1,30 @@
 from __future__ import annotations
+from typing import Callable
 
 
 class Heap:
-    def __init__(self, source: list[int], order="max"):
+    def __init__(self, source: list[int], order="min"):
         n = len(source)
         self.source = source
+
+        def sift_up(self, idx: int):
+            pass
+
+        def sift_down(self, start_idx: int, pos: int):
+            pass
+
+        match order:
+            case "max":
+                self.sift_up: Callable = self._siftup_max
+                self.sift_down: Callable = self._siftdown_max
+            case "min":
+                self.sift_up: Callable = self._siftup_min
+                self.sift_down: Callable = self._siftdown_min
+
         if n == 0:
             return
-
         for i in range(n // 2, -1, -1):
-            self._sift_up(i)
+            self.sift_up(i)
 
     def __len__(self):
         return len(self.source)
@@ -54,7 +69,7 @@ class Heap:
             result += line.center(max_width) + "\n"
         return result
 
-    def _sift_down(self, start_idx: int, pos: int) -> int:
+    def _siftdown_min(self, start_idx: int, pos: int) -> int:
         siftee = self[pos]
 
         while pos > start_idx:
@@ -71,7 +86,24 @@ class Heap:
         self[pos] = siftee
         return pos
 
-    def _sift_up(self, idx):
+    def _siftdown_max(self, start_idx: int, pos: int) -> int:
+        siftee = self[pos]
+
+        while pos > start_idx:
+            parent_pos = (pos - 1) >> 1  # (pos - 1) // 2
+            parent = self[parent_pos]
+
+            if siftee > parent:
+                self[pos] = parent
+                pos = parent_pos
+                continue
+
+            break
+
+        self[pos] = siftee
+        return pos
+
+    def _siftup_min(self, idx):
         upper_bound = len(self)
         start_idx = idx
         child_idx = 2 * idx + 1
@@ -87,7 +119,25 @@ class Heap:
             child_idx = 2 * idx + 1
 
         self[idx] = new_item
-        self._sift_down(start_idx=start_idx, pos=idx)
+        self._siftdown_min(start_idx=start_idx, pos=idx)
+
+    def _siftup_max(self, idx):
+        upper_bound = len(self)
+        start_idx = idx
+        child_idx = 2 * idx + 1
+        new_item = self[idx]
+        while child_idx < upper_bound:
+            sibling_idx = child_idx + 1
+            if sibling_idx < upper_bound and self[sibling_idx] > self[child_idx]:
+                child_idx = sibling_idx
+
+            self[idx] = self[child_idx]
+
+            idx = child_idx
+            child_idx = 2 * idx + 1
+
+        self[idx] = new_item
+        self._siftdown_max(start_idx=start_idx, pos=idx)
 
     def _sift_up_early(self, idx):
         upper_bound = len(self)
@@ -108,7 +158,7 @@ class Heap:
             child_idx = 2 * idx + 1
 
         self[idx] = new_item
-        self._sift_down(start_idx=start_idx, pos=idx)
+        self._siftdown_min(start_idx=start_idx, pos=idx)
 
     def heap_pop(self) -> int:
         leaf = self.source.pop()
@@ -116,7 +166,7 @@ class Heap:
         if self.source:
             return_item = self[0]
             self[0] = leaf
-            self._sift_up(0)
+            self.sift_up(0)
             return return_item
 
         return leaf
@@ -134,4 +184,4 @@ class Heap:
 
     def heap_push(self, value: int):
         self.source.append(value)
-        return self._sift_down(start_idx=0, pos=len(self) - 1)
+        return self.sift_down(start_idx=0, pos=len(self) - 1)
