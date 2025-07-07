e6da80c343ce553eddad9c023367f91cfa09d12b
Francisco Andrade <frma059@gmail.com>
2025-07-07 16:57:56 -0300

    fix offset logic and modularise the sample generation

--- strings/string_matching.py
+++ strings/string_matching.py
@@ -1,13 +1,15 @@
 from collections import defaultdict
+from typing import Optional
+import re
 
 
-def naive_string_match(source: str, pattern: str) -> bool:
+def naive_string_match(source: str, pattern: str) -> tuple[bool, Optional[int]]:
     """
     Check if the source string matches the pattern exactly.
     """
     i, j = 0, 0
 
-    for char in source:
+    for idx, char in enumerate(source):
         if j >= len(pattern):
             break
 
@@ -16,21 +18,48 @@ def naive_string_match(source: str, pattern: str) -> bool:
         if check:
             j += 1
         else:
-            i += 1
+            i = idx + 1
             j = 0
 
-    return j >= len(pattern)
+    result = j >= len(pattern)
+
+    return result, i
 
 
-words = ["grape", "banana", "melon", "apple", "orange", "expensive"]
-patterns = ["ple", "nana", "x", "ap", "lon", "nsive", "p"]
+def extract_sample(
+    path: str, split_pattern: str = r"[\s:;.,()\[\]{}\"'=<>!#@\\/\|\-\+\*\&%$^`~]+"
+) -> list[str]:
+    with open(path, "r", encoding="utf-8") as f:
+        avl_content = f.read()
+    return [w for w in re.split(split_pattern, avl_content) if w]
 
 
-result = defaultdict(list)
+words = extract_sample("search/matrix_search.py")
+patterns = extract_sample("trees/avl_tree.py")
+
+
+result = defaultdict(set)
 for pattern in patterns:
+    result[pattern]
     for word in words:
-        if naive_string_match(source=word, pattern=pattern):
-            result[pattern].append(word)
+        is_match, offset = naive_string_match(source=word, pattern=pattern)
+
+        if is_match:
+            assert word[offset : offset + len(pattern)] == pattern
+            result[pattern].add(word)
 
 for pattern, matches in result.items():
     print(f"Pattern '{pattern}' matches: {', '.join(matches)}")
+
+sorted_patterns = sorted(result.keys(), key=lambda x: len(result[x]))
+for pattern in sorted_patterns:
+    print(f"Pattern '{pattern}' - # of matches: {len(result[pattern])}")
+
+print(
+    "Unmatched patterns: ",
+    len(list(filter(lambda x: len(x) == 0, result.values()))),
+)
+print(
+    "patterns with more than one match: ",
+    len(list(filter(lambda x: len(x) > 0, result.values()))),
+)
