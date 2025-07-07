caf59fac89bdde89f52a0f2f0c3f5690925e361f
Francisco Andrade <frma059@gmail.com>
2025-07-07 20:22:17 -0300

    string matching: horner's rule

--- strings/string_matching.py
+++ strings/string_matching.py
@@ -20,6 +20,15 @@ def naive_string_match(text: str, pattern: str) -> list[int]:
     return result
 
 
+def horner_rule(text: str, idx: int) -> int:
+    if idx < 0 or idx >= len(text):
+        return 0
+    ord_val = ord(text[idx])
+    hr = horner_rule(text, idx - 1)
+    exponent = len(text) - idx - 1
+    return (10**exponent) * ord_val + hr
+
+
 def extract_sample(
     path: str, split_pattern: str = r"[\s:;.,()\[\]{}\"'=<>!#@\\/\|\-\+\*\&%$^`~]+"
 ) -> list[str]:
@@ -28,33 +37,46 @@ def extract_sample(
     return [w for w in re.split(split_pattern, avl_content) if w]
 
 
-words = extract_sample("search/matrix_search.py")
-patterns = extract_sample("trees/avl_tree.py")
+def match_files(
+    text_path: str = "search/matrix_search.py", pattern_path: str = "trees/avl_tree.py"
+):
+    """
+    Match patterns from one file against words in another file.
+    """
+
+    words = extract_sample(text_path)
+    patterns = extract_sample(pattern_path)
+
+    result = defaultdict(set)
+    for pattern in patterns:
+        result[pattern]
+        for word in words:
+            results = naive_string_match(text=word, pattern=pattern)
+            is_match = len(results) > 0
+            if is_match:
+                for offset in results:
+                    assert word[offset : offset + len(pattern)] == pattern
+                result[pattern].add(word)
+
+    for pattern, matches in result.items():
+        print(f"Pattern '{pattern}' matches: {', '.join(matches)}")
+
+    sorted_patterns = sorted(result.keys(), key=lambda x: len(result[x]))
+    for pattern in sorted_patterns:
+        print(f"Pattern '{pattern}' - # of matches: {len(result[pattern])}")
+
+    print(
+        "Unmatched patterns: ",
+        len(list(filter(lambda x: len(x) == 0, result.values()))),
+    )
+    print(
+        "patterns with more than one match: ",
+        len(list(filter(lambda x: len(x) > 0, result.values()))),
+    )
 
 
-result = defaultdict(set)
-for pattern in patterns:
-    result[pattern]
-    for word in words:
-        results = naive_string_match(text=word, pattern=pattern)
-        is_match = len(results) > 0
-        if is_match:
-            for offset in results:
-                assert word[offset : offset + len(pattern)] == pattern
-            result[pattern].add(word)
-
-for pattern, matches in result.items():
-    print(f"Pattern '{pattern}' matches: {', '.join(matches)}")
-
-sorted_patterns = sorted(result.keys(), key=lambda x: len(result[x]))
-for pattern in sorted_patterns:
-    print(f"Pattern '{pattern}' - # of matches: {len(result[pattern])}")
-
-print(
-    "Unmatched patterns: ",
-    len(list(filter(lambda x: len(x) == 0, result.values()))),
-)
-print(
-    "patterns with more than one match: ",
-    len(list(filter(lambda x: len(x) > 0, result.values()))),
-)
+def ex1():
+    pattern = "0001"
+    text = "000010001010001"
+    result = naive_string_match(text, pattern)
+    print(f"Pattern '{pattern}' found at positions: {result}")
