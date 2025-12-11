40111d0cec91e312c3c00a28fae3db5bf1f7d7cc
Francisco Andrade <frma059@gmail.com>
2025-12-10 22:20:02 -0300

    removing prints

--- sorting/heapsort.py
+++ sorting/heapsort.py
@@ -8,15 +8,12 @@ def heapsort(source: list[int]):
     for i in range(n - 1, 0, -1):
         heap[0], heap[i] = heap[i], heap[0]
         heap.limit -= 1
-        print(heap.source[: heap.limit], heap.source[heap.limit :])
         max_heapify(heap, 0)
 
 
 if __name__ == "__main__":
     source = [1, 3, 2, 4, 7, 9, 8, 16, 14, 10]
-
     print(source)
     heapsort(source)
     print(source)
-
     assert source == sorted(source)
